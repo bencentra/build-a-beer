b9ac3ef6c4b73268e6a256129cc2785d88c676e0
Ben Centra <blcentra@gmail.com>
2017-03-27 22:09:32 -0400

    example calculation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import brew from 'brew-eq';
+
+// ==============================
+// Calculating the mash bill
+// ==============================
+
+let finalGravityInput, finalVolumeInput, totalGravitySpan;
+
+const calculateTotalGUs = () => {
+  const finalGravity = brew.toGU(Number.parseFloat(finalGravityInput.value)) || 0;
+  const finalVolume = Number.parseFloat(finalVolumeInput.value) || 0;
+  const totalGravity = Number.parseInt(brew.totalGravity(finalVolume, finalGravity));
+  console.log(finalGravity, finalVolume, totalGravity);
+  totalGravitySpan.innerText = totalGravity;
+};
+
+finalGravityInput = document.querySelector('#final-gravity')
+finalGravityInput.addEventListener('keyup', calculateTotalGUs);
+
+finalVolumeInput = document.querySelector('#final-volume')
+finalVolumeInput.addEventListener('keyup', calculateTotalGUs);
+
+totalGravitySpan = document.querySelector('#total-gravity');
