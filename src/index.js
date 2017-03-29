a93cbcc064074c426806a5fdbe7abd956c08f1b5
Ben Centra <blcentra@gmail.com>
2017-03-28 22:42:41 -0400

    trying out vue.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,78 @@
+import Vue from 'vue';
 import brew from 'brew-eq';
 
 // ==============================
 // Calculating the mash bill
 // ==============================
 
-let finalGravityInput, finalVolumeInput, totalGravitySpan;
-
-const calculateTotalGUs = () => {
-  const finalGravity = brew.toGU(Number.parseFloat(finalGravityInput.value)) || 0;
-  const finalVolume = Number.parseFloat(finalVolumeInput.value) || 0;
-  const totalGravity = Number.parseInt(brew.totalGravity(finalVolume, finalGravity));
-  console.log(finalGravity, finalVolume, totalGravity);
-  totalGravitySpan.innerText = totalGravity;
+const finalGravityInput = {
+  id: 'final-gravity',
+  label: 'Final Gravity',
+  placeholder: 'e.g. 1.052',
+  value: ''
 };
 
-finalGravityInput = document.querySelector('#final-gravity')
-finalGravityInput.addEventListener('keyup', calculateTotalGUs);
+const finalVolumeInput = {
+  id: 'final-volume',
+  label: 'Final Volume',
+  placeholder: 'e.g. 5.5 gallons',
+  value: ''
+};
 
-finalVolumeInput = document.querySelector('#final-volume')
-finalVolumeInput.addEventListener('keyup', calculateTotalGUs);
+const totalExtractFormConfig = {
+  title: "Total Extract Per Batch",
+  prefix: 'te',
+  inputs: [finalGravityInput, finalVolumeInput],
+  result: {
+    name: 'Total Gravity',
+    units: 'GU'
+  }
+};
 
-totalGravitySpan = document.querySelector('#total-gravity');
+Vue.component('form-group', {
+  props: ['input'],
+  template:
+`
+<div class="form-group row">
+  <label v-bind:for="input.id" class="col-2 col-form-label">{{ input.label }}</label>
+  <div class="col-10">
+    <input class="form-control" type="text" v-bind:placeholder="input.placeholder" v-model="input.value" v-bind:id="input.id">
+  </div>
+</div>
+`
+});
+
+Vue.component('card', {
+  props: ['form', 'value'],
+  template:
+`
+<div class="card">
+  <div class="card-header">{{ form.title }}</div>
+  <div class="card-block">
+    <form-group v-for="input in form.inputs" v-bind:input="input"></form-group>
+    <div class="row">
+      <div class="col-2">{{ form.result.name }}</div>
+      <div class="col-10"><span id="total-gravity">{{ value }}</span> {{form.result.units}}</div>
+    </div>
+  </div>
+</div>
+`
+});
+
+const Form = Vue.extend({});
+
+const totalExtractForm = new Form({
+  el: '#total-extract',
+  data: {
+    config: totalExtractFormConfig
+  },
+  computed: {
+    value: function() {
+      const finalGravity = brew.toGU(Number.parseFloat(finalGravityInput.value)) || 0;
+      const finalVolume = Number.parseFloat(finalVolumeInput.value) || 0;
+      const totalGravity = Number.parseInt(brew.totalGravity(finalVolume, finalGravity));
+      console.log(finalGravity, finalVolume, totalGravity);
+      return totalGravity;
+    }
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
   }
 };
